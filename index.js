6af0cc50d1ff3b03ac847d65eb40c00a5e0fae32
Luxper <164402605+Luxper@users.noreply.github.com>
2024-03-26 16:28:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+JavaScript para hacer que la navegación sea pegajosa (sick)
+window adver itener'scro, funcion $
+const header = document querySelectortheader),
+if (window. scrlTY > header ofsetight 4
+reader clsstistadd(stcky]:
+)elsa
+header classListramove (sy
+)
+»
+
+11 JavaScript para armar el desplazamiento suave al hecar ci on aieces de ravegación
+ocument querySelectorAlnaw 2) JorEachianchor => (
+‘anchor addEventListener( cick, funcion(e)
+‘.preveniDefaui:
+
+‘cont targetld = this getAttibute( heat):
+“const largersiament = document querySetectortargeni):
+
+window seralk{
+‘op: targetElementoffsatTop - document. querySelector healer) offsatHaight,
+behavor 'smooti
+»
+»
+»
+
